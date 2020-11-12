13573629a58461f9f82697cffc1c2114458da719
Prasenjit Manna <prmanna@cisco.com>
2020-11-13 00:15:42 +0530

    Initial commit

--- MultiLayerNN.py
+++ MultiLayerNN.py
@@ -7,8 +7,52 @@
 import numpy as np
 from sklearn.datasets import make_moons
 from sklearn.model_selection import train_test_split
+from random import seed
+from random import randrange
+from random import random
+from csv import reader
+from math import exp
 import matplotlib.pyplot as plt
 #%matplotlib inline
+
+# Load a CSV file
+def load_csv(filename):
+	dataset = list()
+	with open(filename, 'r') as file:
+		csv_reader = reader(file)
+		for row in csv_reader:
+			if not row:
+				continue
+			dataset.append(row)
+	return dataset
+
+# Convert string column to float
+def str_column_to_float(dataset, column):
+	for row in dataset:
+		row[column] = float(row[column].strip())
+
+# Convert string column to integer
+def str_column_to_int(dataset, column):
+	class_values = [row[column] for row in dataset]
+	unique = set(class_values)
+	lookup = dict()
+	for i, value in enumerate(unique):
+		lookup[value] = i
+	for row in dataset:
+		row[column] = lookup[row[column]]
+	return lookup
+
+# Find the min and max values for each column
+def dataset_minmax(dataset):
+	minmax = list()
+	stats = [[min(column), max(column)] for column in zip(*dataset)]
+	return stats
+
+# Rescale dataset columns to the range 0-1
+def normalize_dataset(dataset, minmax):
+	for row in dataset:
+		for i in range(len(row)-1):
+			row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
 
 # %%
 """
@@ -112,6 +156,10 @@ plt.show()
 """
 ## Creating a Network, Training it on our Moons Data, Testing the model
 """
+
+# load and prepare data
+filename = 'wheat-seeds.csv'
+dataset = load_csv(filename)
 
 # %%
 weights=create_arch(X,3,1)
